6981dd7c8ba7edb7bf77cf6b3a88584f1b3fbb9f
KrawezG <kozlovivan1855@gmail.com>
2023-12-16 21:30:11 +0300

    2

--- 2_dz/2.v
+++ 2_dz/2.v
@@ -3,20 +3,27 @@
 
 module complex_mult (
 	input clk,  
-	input signed  [15:0] x, 
-	output signed [15:0] y 
+	input signed  [7:0] Re_in_1, 
+    input signed  [7:0] Re_in_2, 
+    input signed  [7:0] Im_in_1, 
+    input signed  [7:0] IM-in_2, 
+    input signed [1:0] data_valid; 
+    output signed  [7:0] Re_out, 
+    output signed  [7:0] Im_out, 
 );
+
+
 endmodule
 
 module mult (
 	input clk, 
-	input signed [15:0] num1,
-	input signed [15:0] num2,
-	output signed [15:0] res	
+	input signed [7:0] num1,
+	input signed [7:0] num2,
+	output signed [7:0] res	
 );
 
-reg signed [15:0] mult;
-reg signed [15:0] tmp;
+reg signed [7:0] mult;
+reg signed [7:0] tmp;
 
 always @(posedge clk) begin
 	tmp <= num1 * num2;
--- 2_dz/2_tb.v
+++ 2_dz/2_tb.v
@@ -1,1 +1,98 @@
-`timescale 1ns/1ns
+`timescale 1ns / 1ns
+
+module complex_tb();
+
+reg clk = 0;
+reg signed [7:0] a_real = 0;
+reg signed [7:0] a_imag = 0;
+reg signed [7:0] b_real = 0;
+reg signed [7:0] b_imag = 0;
+reg signed [1:0] data_valid = 0; 
+wire signed [15:0] z_real;
+wire signed [15:0] z_imag;
+
+initial
+    forever #5 clk = ~clk;
+
+initial
+begin
+
+    a_real <= 8'sd1; 
+    a_imag <= 8'sd2;
+    b_real <= 8'sd3;
+    b_imag <= 8'sd4;
+    data_valid <= 2'b01; 
+
+    #10;
+    data_valid <= 2'b00; 
+    a_real <= 8'sd00; 
+    a_imag <= 8'sd00;
+    b_real <= 8'sd00;
+    b_imag <= 8'sd00;
+    
+    
+    #20;
+    a_real <= 8'sd02; 
+    a_imag <= 8'sd04;
+    b_real <= 8'sd06;
+    b_imag <= 8'sd08;
+    data_valid <= 2'b01; 
+
+    #10;
+    data_valid <= 2'b00; 
+    a_real <= 8'sd00; 
+    a_imag <= 8'sd00;
+    b_real <= 8'sd00;
+    b_imag <= 8'sd00;
+
+    #20;
+    a_real <= 8'sd01; 
+    a_imag <= 8'sd03;
+    b_real <= 8'sd05;
+    b_imag <= 8'sd07;
+    data_valid <= 2'b01; 
+
+    #10;
+    data_valid <= 2'b00; 
+    a_real <= 8'sd00; 
+    a_imag <= 8'sd00;
+    b_real <= 8'sd00;
+    b_imag <= 8'sd00;
+
+    #20;
+    a_real <= 8'sd01; 
+    a_imag <= 8'sd02;
+    b_real <= 8'sd01;
+    b_imag <= 8'sd02;
+    data_valid <= 2'b01; 
+
+    #10;
+    data_valid <= 2'b00; 
+    a_real <= 8'sd00; 
+    a_imag <= 8'sd00;
+    b_real <= 8'sd00;
+    b_imag <= 8'sd00;
+
+    #200;
+
+    $finish;
+end
+
+complex dut (
+    .clk(clk), 
+    .a_real(a_real), 
+    .a_imag(a_imag), 
+    .b_real(b_real), 
+    .b_imag(b_imag), 
+    .data_valid(data_valid), 
+    .z_real(z_real), 
+    .z_imag(z_imag)
+);
+
+initial
+begin
+    $dumpfile("test.vsd");
+    $dumpvars;
+end
+
+endmodule
