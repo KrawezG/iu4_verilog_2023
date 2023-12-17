751358bc9e412c0dfc2dfbdbc0d5a6dcb1d7b6e2
KrawezG <kozlovivan1855@gmail.com>
2023-12-18 00:58:47 +0300

    Pocti gotovo

--- 2_dz/2.v
+++ 2_dz/2.v
@@ -1,33 +1,147 @@
 
 
 
-module complex_mult (
+module Complex_Multiplier (
 	input clk,  
-	input signed  [7:0] Re_in_1, 
+	input signed  [7:0] Re_in_1,
+	input signed  [7:0] Im_in_1, 
     input signed  [7:0] Re_in_2, 
-    input signed  [7:0] Im_in_1, 
-    input signed  [7:0] IM-in_2, 
-    input signed [1:0] data_valid; 
-    output signed  [7:0] Re_out, 
-    output signed  [7:0] Im_out, 
+    input signed  [7:0] Im_in_2, 
+    input signed [1:0] data_valid_in,
+    output signed  [15:0] Re_out, 
+    output signed  [15:0] Im_out,
+	output signed  [1:0] data_valid_out
 );
 
+reg [7:0] a = 0;
+reg [7:0] b = 0;
+reg [7:0] c = 0;
+reg [7:0] d = 0;
+reg [15:0] k1 = 9;
+reg [15:0] k2 = 9;
+reg [15:0] k3 = 9;
+reg [15:0] x = 0;
+reg [15:0] y = 0;
+reg [2:0] state = 0;
+reg [1:0] flag = 0;
+reg [3:0] cnt = 0;
+//reg [1:0] data_good_in = 0;
+reg [1:0] data_good_out = 0;
+
+
+  
+
+reg signed [7:0] operand1 = 0; 
+reg signed [7:0] operand2 = 0;
+reg signed [15:0] result = 0; 
+
+  	always @(posedge clk) begin
+		result <= operand1 * operand2;
+    end
+
+
+  	always @(posedge clk) begin
+	//data_good_in <= data_valid_in;
+    if (data_valid_in) 
+	begin
+		a <= Re_in_1;
+		b <= Im_in_1;
+		c <= Re_in_2;
+		d <= Im_in_2;
+		state <= 1;
+		//data_good_out <= 0;
+		k1 <= result;
+		flag <= 1;
+	end
+	// if (!data_valid_in && flag == 1 ) 
+	// begin
+	// 	flag <= 0;
+	// 	data_good_out <= 0;
+	// 	state <= 1;
+	// 	//k1 <= result;
+	// end
+
+	case (state)
+		1: begin
+		// k1 = c * (a - b)
+		operand1 <= c;
+		operand2 <= (b - a);
+		k2 <= result;
+		data_good_out <= 0;
+
+		// Move to the next state
+		state <= state + 1;
+		end
+		2: begin
+		// k2 = b * (c - d)
+		operand1 <= b;
+		operand2 <= (c - d);
+		k3 <= result;
+
+		// Move to the next state
+		state <= state + 1;
+		// flag <= 1;
+		end
+		3: begin
+		// k3 = a * (c + d)
+		operand1 <= a;
+		operand2 <= (c + d);
+		k1 <= result;
+
+		// x = k2 - k1
+		x <= k2 - k1;
+
+		// y = k1 + k3
+		y <= k1 + k3;
+		//state <= 0;
+		// if (flag == 0) 
+		// begin
+		
+		// 	flag <= 1;
+		// end && !data_valid_in
+		if (flag == 1 && data_valid_in) 
+		begin
+		end else
+		if (flag == 1 ) 
+		begin
+			state <= 1;
+			flag <= 0;
+		end else 
+		begin
+			state <= 0;
+		end
+
+		if (flag == 1 && cnt < 1) 
+		begin
+			cnt <= cnt + 1;
+		end else
+		if (flag == 1 && cnt < 2) 
+		begin
+			data_good_out <= 1;
+			cnt <= cnt + 1;
+		end else
+		if (flag == 1 && cnt == 2) 
+		begin
+			data_good_out <= 1;
+		end else
+		begin
+			data_good_out <= 1;
+			cnt <= 0;
+		end
+
+		//data_good_out <= 1;
+		end
+		default: begin
+		data_good_out <= 0;
+		end
+	endcase
+    
+  end
+  
+  assign Re_out = x;
+  assign Im_out = y;
+  assign data_valid_out = data_good_out;
 
 endmodule
 
-module mult (
-	input clk, 
-	input signed [7:0] num1,
-	input signed [7:0] num2,
-	output signed [7:0] res	
-);
 
-reg signed [7:0] mult;
-reg signed [7:0] tmp;
-
-always @(posedge clk) begin
-	tmp <= num1 * num2;
-	mult <= tmp;
-end
-assign res = mult;
-endmodule
--- 2_dz/2_tb.v
+++ 2_dz/2_tb.v
@@ -1,92 +1,102 @@
 `timescale 1ns / 1ns
 
-module complex_tb();
+module complex_mult_tb();
 
 reg clk = 0;
-reg signed [7:0] a_real = 0;
-reg signed [7:0] a_imag = 0;
-reg signed [7:0] b_real = 0;
-reg signed [7:0] b_imag = 0;
-reg signed [1:0] data_valid = 0; 
-wire signed [15:0] z_real;
-wire signed [15:0] z_imag;
-
+reg signed [7:0] Re_in_1 = 0;
+reg signed [7:0] Im_in_1 = 0;
+reg signed [7:0] Re_in_2 = 0;
+reg signed [7:0] Im_in_2 = 0;
+reg signed [1:0] data_valid_in = 0; 
+wire signed [15:0] Re_out;
+wire signed [15:0] Im_out;
+wire signed [1:0] data_valid_out;
 initial
     forever #5 clk = ~clk;
 
 initial
 begin
-
-    a_real <= 8'sd1; 
-    a_imag <= 8'sd2;
-    b_real <= 8'sd3;
-    b_imag <= 8'sd4;
-    data_valid <= 2'b01; 
+    //first test (1+0i)*(1+0i)
+    Re_in_1 <= 8'sd03; 
+    Im_in_1 <= 8'sd01;
+    Re_in_2 <= 8'sd03;
+    Im_in_2 <= 8'sd01;
+    data_valid_in <= 2'b01; 
 
     #10;
-    data_valid <= 2'b00; 
-    a_real <= 8'sd00; 
-    a_imag <= 8'sd00;
-    b_real <= 8'sd00;
-    b_imag <= 8'sd00;
+
+    data_valid_in <= 2'b00; 
+    Re_in_1 <= 8'sd00; 
+    Im_in_1 <= 8'sd00;
+    Re_in_2 <= 8'sd00;
+    Im_in_2 <= 8'sd00;
+
+    #20;
+
+    //second test (0+1i)*(0+1i)
+    Re_in_1 <= 8'sd02; 
+    Im_in_1 <= 8'sd01;
+    Re_in_2 <= 8'sd02;
+    Im_in_2 <= 8'sd01;
+    data_valid_in <= 2'b01;
+
+    #10;
+
+    data_valid_in <= 2'b00; 
+    Re_in_1 <= 8'sd00; 
+    Im_in_1 <= 8'sd00;
+    Re_in_2 <= 8'sd00;
+    Im_in_2 <= 8'sd00;
+
+    #20;
+
+    //third test (1+1i)*(1+1i)
+    Re_in_1 <= 8'sd01; 
+    Im_in_1 <= 8'sd01;
+    Re_in_2 <= 8'sd01;
+    Im_in_2 <= 8'sd01;
+    data_valid_in <= 2'b01; 
+
+    #10;
+
+    data_valid_in <= 2'b00; 
+    Re_in_1 <= 8'sd00; 
+    Im_in_1 <= 8'sd00;
+    Re_in_2 <= 8'sd00;
+    Im_in_2 <= 8'sd00;
+
+    #20;
+
+    //fourth test (1+2i)*(1+2i)
+    Re_in_1 <= 8'sd01; 
+    Im_in_1 <= 8'sd02;
+    Re_in_2 <= 8'sd01;
+    Im_in_2 <= 8'sd02;
+    data_valid_in <= 2'b01;
+
+    #10;
     
-    
-    #20;
-    a_real <= 8'sd02; 
-    a_imag <= 8'sd04;
-    b_real <= 8'sd06;
-    b_imag <= 8'sd08;
-    data_valid <= 2'b01; 
-
-    #10;
-    data_valid <= 2'b00; 
-    a_real <= 8'sd00; 
-    a_imag <= 8'sd00;
-    b_real <= 8'sd00;
-    b_imag <= 8'sd00;
-
-    #20;
-    a_real <= 8'sd01; 
-    a_imag <= 8'sd03;
-    b_real <= 8'sd05;
-    b_imag <= 8'sd07;
-    data_valid <= 2'b01; 
-
-    #10;
-    data_valid <= 2'b00; 
-    a_real <= 8'sd00; 
-    a_imag <= 8'sd00;
-    b_real <= 8'sd00;
-    b_imag <= 8'sd00;
-
-    #20;
-    a_real <= 8'sd01; 
-    a_imag <= 8'sd02;
-    b_real <= 8'sd01;
-    b_imag <= 8'sd02;
-    data_valid <= 2'b01; 
-
-    #10;
-    data_valid <= 2'b00; 
-    a_real <= 8'sd00; 
-    a_imag <= 8'sd00;
-    b_real <= 8'sd00;
-    b_imag <= 8'sd00;
+    data_valid_in <= 2'b00; 
+    Re_in_1 <= 8'sd00; 
+    Im_in_1 <= 8'sd00;
+    Re_in_2 <= 8'sd00;
+    Im_in_2 <= 8'sd00;
 
     #200;
 
     $finish;
 end
 
-complex dut (
+Complex_Multiplier dut (
     .clk(clk), 
-    .a_real(a_real), 
-    .a_imag(a_imag), 
-    .b_real(b_real), 
-    .b_imag(b_imag), 
-    .data_valid(data_valid), 
-    .z_real(z_real), 
-    .z_imag(z_imag)
+    .Re_in_1(Re_in_1), 
+    .Im_in_1(Im_in_1), 
+    .Re_in_2(Re_in_2), 
+    .Im_in_2(Im_in_2), 
+    .data_valid_in(data_valid_in), 
+    .Re_out(Re_out), 
+    .Im_out(Im_out),
+    .data_valid_out(data_valid_out)
 );
 
 initial
